b000c1d8b7fd316c1a1d7e2f243ac2e3c54ab8eb
fantasykan <92788128+fantasykan@users.noreply.github.com>
2024-04-03 22:19:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,9 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-git pull
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#git pull
 
-
+sed -i '1i src-git packages https://github.com/immortalwrt/packages.git' feeds.conf.default
+sed -i '2i src-git luci https://github.com/immortalwrt/luci.git' feeds.conf.default
