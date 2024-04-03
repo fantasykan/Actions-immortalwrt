a71b4f91696fee9f25fb91d67d630916b159ad21
fantasykan <92788128+fantasykan@users.noreply.github.com>
2024-04-03 22:47:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,12 +14,20 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #git pull
 
-sed -i '1i src-git packages https://github.com/immortalwrt/packages.git' feeds.conf.default
-sed -i '2i src-git luci https://github.com/immortalwrt/luci.git' feeds.conf.default
+printf '
+src-git packages https://github.com/immortalwrt/packages.git
+src-git luci https://github.com/immortalwrt/luci.git
+src-git routing https://github.com/openwrt/routing.git
+src-git telephony https://github.com/openwrt/telephony.git
+#src-git-full video https://github.com/openwrt/video.git
+#src-git-full targets https://github.com/openwrt/targets.git
+#src-git-full oldpackages http://git.openwrt.org/packages.git
+#src-link custom /usr/src/openwrt/custom-feed
+' > feeds.conf.default
