0b4163a4b50aab4779d18d69bad7bcc99dde686e
fantasykan <92788128+fantasykan@users.noreply.github.com>
2024-04-08 12:38:20 +0800

    imm_kenzok9

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,7 +31,7 @@ src-git telephony https://github.com/openwrt/telephony.git
 #src-git-full oldpackages http://git.openwrt.org/packages.git
 #src-link custom /usr/src/openwrt/custom-feed
 #src-git smpackage https://github.com/kenzok8/small-package
-#' > feeds.conf.default
+' > feeds.conf.default
 
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
